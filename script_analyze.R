ff758e6ecbaa0f6fd3a4320875f10bb69779167a
Daniel <daniel.alvaca@gmail.com>
2022-11-14 23:09:12 +0100

    feat: gt table types x gen

--- script_analyze.R
+++ script_analyze.R
@@ -30,7 +30,7 @@ df <- df_pkm %>%
 ### Types (pokemon-wise)
 mat1 <- df %>% 
   distinct(name, type_1, .keep_all = T) %>%
-  tabyl(gen, type_1) %>%
+  tabyl(gen, type_1) %>% select(-NA_) %>%
   select(-1) %>% as.matrix()
 
 mat2 <- df %>%
@@ -39,13 +39,42 @@ mat2 <- df %>%
   select(-1) %>% as.matrix()
 dim(mat1) == dim(mat2)
 
-tab_type_gen <- mat1+mat2 %>% as_tibble()
-tab_type_gen %>% gt
+tab_type_ind <- mat1+mat2
+tab_type_ind <- t(tab_type_ind)
+colnames(tab_type_ind) <- tabyl(df, gen)[[1]]
+tab_type_ind
+
+tab_type_ind_html <- tab_type_ind %>%
+  as_tibble(rownames = "Type") %>%
+  adorn_totals(where = "col") %>%
+  gt %>%
+  tab_spanner(label = "Generation", columns = c(2:11)) %>%
+  cols_align(columns = c(2:11), align = "center") %>%
+  cols_label(H = "H*") %>%
+  data_color(
+    columns = c(2:11),
+    colors = scales::col_numeric(
+      palette = pals::cividis(n = 10),#pals::ocean.haline(n = 10),
+      reverse = T,
+      domain = NULL
+    ), #alpha = 0.85
+  ) %>%
+  # data_color(
+  #   columns = c(12),
+  #   colors = scales::col_numeric(
+  #     palette = pals::cividis(n = 20)[1:10],#pals::ocean.haline(n = 10),
+  #     reverse = T,
+  #     domain = NULL
+  #   ), #alpha = 0.85
+  # ) %>%
+  tab_source_note(source_note = "*Hisui")
+
+gtsave(tab_type_ind_html, filename = "out/Types by generation (n of pokémons).html")
 
 ### Types (line-wise)
 mat1 <- df %>% 
   distinct(line, type_1, .keep_all = T) %>%
-  tabyl(gen, type_1) %>%
+  tabyl(gen, type_1) %>% select(-NA_) %>%
   select(-1) %>% as.matrix()
 
 mat2 <- df %>%
@@ -54,18 +83,20 @@ mat2 <- df %>%
   select(-1) %>% as.matrix()
 dim(mat1) == dim(mat2)
 
-tab_type_gen <- mat1+mat2 %>% as_tibble()
-tab_type_gen %>% gt
+tab_type_gen <- mat1+mat2
+tab_type_gen <- t(tab_type_gen)
+colnames(tab_type_gen) <- tabyl(df, gen)[[1]]
+tab_type_gen
 
+input <- tab_type_gen %>% as_tibble(rownames = "Type") %>% adorn_totals(where = "col") %>% gt
 
-
-
-
-
-# To-do -------------------------------------------------------------------
-
-#Git repository
-#Check sex percentage of rare mons
-#Add Mythical mons
-
+input %>% data_color(
+  columns = c(2:11),
+  colors = scales::col_numeric(
+    palette = pals::cividis(n = 10),#pals::ocean.haline(n = 10),
+    reverse = T,
+    domain = NULL
+  ),
+  #alpha = 0.85
+)
 
